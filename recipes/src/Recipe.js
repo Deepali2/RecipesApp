f7437dc67d33296dd5cff86617864b635a9a716f
Deepali2 <hdgarg@gmail.com>
2018-12-09 19:14:53 -0800

    add image, methid to the Recipe component; style the Recipe Component

--- recipes/src/Recipe.js
+++ recipes/src/Recipe.js
@@ -3,16 +3,24 @@ import './Recipe.css';
 
 class Recipe extends Component {
   render() {
-    const {title, ingredients} = this.props;
+    const {title, ingredients, img, method} = this.props;
     
     return (
-      <div>
-        <div>Recipe {title}</div>
-        <ul>Ingredients:
-           {ingredients.map((ingredient, index) => {
-             return <li key={index}>{ingredient}</li>
-           })}
-        </ul>
+      <div className='recipe-card'>
+        <div className='recipe-card-img'>
+          <img src={img} alt={title} />
+        </div>
+        <div className='recipe-card-content'>
+          <h3 className='recipe-title'>{title}</h3>
+          <h4>Ingredients:</h4>
+          <ul>
+            {ingredients.map((ingredient, index) => {
+              return <li key={index}>{ingredient}</li>
+            })}
+          </ul>
+          <h4>Method</h4>
+          <p> {method}</p>
+        </div>        
       </div>
     );
   }
--- recipes/src/RecipeApp.js
+++ recipes/src/RecipeApp.js
@@ -7,7 +7,24 @@ class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
-         <Recipe title='pasta' ingredients={['flour', 'water']}/>
+         <Recipe 
+         title='Gulab Jamun' 
+         ingredients={
+           [
+           '1 cup Bisquick (regular not low fat)- not heaped a little towards the less side', 
+           '3 cups Milkpowder- not heaped a little towards the less side- Nestle brand is preferable',
+           'A pinch of Baking Soda',
+           'Whole Milk or 2% milk (around 1 cup)',
+           'Sugar (1 ¾ cup sugar)',
+           'Water (2 cups)',
+           'Heavy Whipping Cream (around 1 cup)'
+           ]
+         }
+         method="Balls: Take 3 parts milkpowder to I part Bisquick in a big bowl. Add a pinch of baking soda. Mix well. Take heavy whipping cream and 2% milk in 1:1 ratio. If using whole milk then decrease the quantity of heavy whipping cream slightly. Warm 30 seconds in microwave. Add the cream and milk solution to the dry ingredients slowly, mixing the dry ingredients to make a loose dough. Let the dough sit for half hour covered. Now knead the dough loosely again. If needed add some more of the cream milk solution. Make small smooth balls pressing using palm of hand. Fry the balls in medium to low heat. Put in sugar syrup.
+         Sugar Syrup: Take sugar in a bowl. Add water. Heat till the sugar dissolves and slightly more. Do not boil the sugar solution.
+         "
+         img='https://i.imgur.com/WsKNvgq.jpg'
+         />
       </div>
     );
   }
