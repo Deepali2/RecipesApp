0003f4e359f7c49a04bf0ec05f6ecd536dafa027
Deepali2 <hdgarg@gmail.com>
2018-12-10 09:21:58 -0800

    fix method content in Gulab Jamun Recipe

--- recipes/src/RecipeList.js
+++ recipes/src/RecipeList.js
@@ -19,8 +19,10 @@ class RecipeList extends Component {
          'Water (2 cups)',
          'Heavy Whipping Cream (around 1 cup)'
          ],
-
-        method: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
+        method:`Balls: Take 3 parts milkpowder to 1 part Bisquick in a big bowl. Add a pinch of baking soda. Mix well. Take heavy whipping cream and 2% milk in 1:1 ratio. If using whole milk then decrease the quantity of heavy whipping cream slightly. Warm 30 seconds in microwave. Add the cream and milk solution to the dry ingredients slowly, mixing the dry ingredients to make a loose dough. Let the dough sit for half hour covered. Now knead the dough loosely again. If needed add some more of the cream milk solution. Make small smooth balls pressing using palm of hand. Fry the balls in medium to low heat. Put in sugar syrup. 
+        
+        Sugar Syrup: Take sugar in a bowl. Add water. Heat till the sugar dissolves and slightly more. Do not boil the sugar solution.`,
+        
       },
       {
         img: 'ghevar.jpg',
