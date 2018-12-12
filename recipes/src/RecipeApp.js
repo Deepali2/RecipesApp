23d8d2a1a858d3fc13304edf2b697f8e7997609d
Deepali2 <hdgarg@gmail.com>
2018-12-12 15:26:09 -0800

    add state to the RecipeApp Component

--- recipes/src/RecipeApp.js
+++ recipes/src/RecipeApp.js
@@ -6,11 +6,52 @@ import Navbar from './Navbar';
 import RecipeList from './RecipeList';
 
 class RecipeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 1,
+          img: 'https://i.imgur.com/WsKNvgq.jpg',
+          title: 'Gulab Jamun',
+          ingredients: 
+          [
+           '1 cup Bisquick (regular not low fat)- not heaped a little towards the less side', 
+           '3 cups Milkpowder- not heaped a little towards the less side- Nestle brand is preferable',
+           'A pinch of Baking Soda',
+           'Whole Milk or 2% milk (around 1 cup)',
+           'Sugar (1 ¾ cup sugar)',
+           'Water (2 cups)',
+           'Heavy Whipping Cream (around 1 cup)'
+           ],
+          method:`Balls: Take 3 parts milkpowder to 1 part Bisquick in a big bowl. Add a pinch of baking soda. Mix well. Take heavy whipping cream and 2% milk in 1:1 ratio. If using whole milk then decrease the quantity of heavy whipping cream slightly. Warm 30 seconds in microwave. Add the cream and milk solution to the dry ingredients slowly, mixing the dry ingredients to make a loose dough. Let the dough sit for half hour covered. Now knead the dough loosely again. If needed add some more of the cream milk solution. Make small smooth balls pressing using palm of hand. Fry the balls in medium to low heat. Put in sugar syrup. 
+          
+          Sugar Syrup: Take sugar in a bowl. Add water. Heat till the sugar dissolves and slightly more. Do not boil the sugar solution.`,
+          
+        },
+        {
+          id: 2,
+          img: 'ghevar.jpg',
+          title: 'Ghevar',
+          ingredients: ['pasta', 'water', 'tomatoes'],
+          method: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
+        },
+        {
+          id: 3,
+          img: 'imgs/photo.jpg',
+          title: 'Rasgulla',
+          ingredients: ['pasta', 'water', 'tomatoes'],
+          method: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
+        }
+      ],
+      nextRecipeId: 4,
+    };
+  }
   render() {
     return (
       <div className="App">
       <Navbar />
-      <RecipeList />
+      <RecipeList recipes={this.state.recipes}/>
       </div>
     );
   }
